da7c1de16ba9a890c3d36a1e4b42cb386e877ce8
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-05 10:55:40 -0300

    Final codigo

--- ExtremalOptimization.py
+++ ExtremalOptimization.py
@@ -28,18 +28,45 @@ else:
     print('Error en la entrada de los parametros')
     print('Los paramentros a ingresar son: semila, archivo de entrada, numero de interaciones y Tau (τ)')
     sys.exit(0)
-    
-np.random.seed(seed)
 
-ncz = np.genfromtxt(archivo_entrada, delimiter=' ', skip_header = 1 , usecols=(1) , skip_footer=2596, dtype = int)            # primer n=nodos c=capacidad z=mejor_solucion en el archivo
-knapback = np.genfromtxt(archivo_entrada, delimiter=',', skip_header = 5 , usecols=(1, 2, 3) , skip_footer=2575, dtype = int) # valor, peso, asignacion
-while True: # Solucion inicial factible, mejorable para capacidades pequeñas
+ite = num_ite
+
+list = []
+count = 0
+while count < seed:
+    np.random.seed(count)
+    num_ite =  ite 
+    ncz = np.genfromtxt(archivo_entrada, delimiter=' ', skip_header = 1 , usecols=(1) , skip_footer=2596, dtype = int)            # primer n=nodos c=capacidad z=mejor_solucion en el archivo
+    knapback = np.genfromtxt(archivo_entrada, delimiter=',', skip_header = 5 , usecols=(1, 2, 3) , skip_footer=2575, dtype = int) # valor, peso, asignacion
     initial_solution = np.random.randint(2, size=int(ncz[0]))
-    if factibility():
-        break
-probabilities = (np.arange(int(ncz[0])) + 1)**-tau
-# np.random.choice(np.arange(int(ncz[0])), 1, p=probabilities/np.sum(probabilities)) # AUN SIN USAR, elegir por metodo de la ruleta
-fitness = create_fitness()
+    while True: # Solucion inicial factible, mejorable para capacidades pequeñas
+        if not factibility():
+            initial_solution[np.random.randint(int(ncz[0]), size=1)] = 0
+        else:
+            break
 
+    probabilities = (np.arange(int(ncz[0])) + 1)**-tau 
+    fitness = create_fitness()
+    fitness_ordenado = np.sort(fitness)
+
+    while 0 < num_ite  and np.sum(initial_solution*knapback[:, 0]) <ncz[2]:
+        elegido =  np.random.choice(fitness_ordenado, 1, p=probabilities/np.sum(probabilities)) # AUN SIN USAR, elegir por metodo de la ruleta
+        valor_indice = np.where(fitness == elegido)
+        valor_indice_random = np.random.choice(valor_indice[0],1)
+        if(initial_solution[valor_indice_random]==0):
+            initial_solution[valor_indice_random]=1
+            if np.sum(initial_solution*knapback[:,1])>ncz[1]:
+                initial_solution[valor_indice_random]=0
+        else:
+            initial_solution[valor_indice_random]=0
+            if np.sum(initial_solution*knapback[:,1])>ncz[1]:
+                initial_solution[valor_indice_random]=1
+        num_ite -= 1
+
+    list.append([count,np.sum(initial_solution*knapback[:,0]), ite-num_ite])
+    count+=1
+
+
+print(np.array(sorted(list, key=lambda list1: list1[2])))
 end = time.time()
 print('Tiempo de ejecución:', end - start,'segundos')
